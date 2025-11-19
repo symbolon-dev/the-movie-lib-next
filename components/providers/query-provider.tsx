43cde999b4e890c92246dc0d20a021fae2d17f66
symbolon-dev <44700110+symbolon-dev@users.noreply.github.com>
2025-11-19 15:40:48 +0100

    Add MovieFilterSkeleton component and implement Suspense for loading states in Home page

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -2,22 +2,19 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { type ReactNode, useState } from 'react';
+import { type ReactNode } from 'react';
 
 export const QueryProvider = ({ children }: { children: ReactNode }) => {
-    const [queryClient] = useState(
-        () =>
-            new QueryClient({
-                defaultOptions: {
-                    queries: {
-                        staleTime: 60000, // 1 Minute
-                        refetchOnWindowFocus: false,
-                        refetchOnReconnect: false,
-                        retry: 1,
-                    },
-                },
-            }),
-    );
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 60000, // 1 Minute
+                refetchOnWindowFocus: false,
+                refetchOnReconnect: false,
+                retry: 1,
+            },
+        },
+    });
 
     return (
         <QueryClientProvider client={queryClient}>
